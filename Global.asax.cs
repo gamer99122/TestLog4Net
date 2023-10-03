0e3b037589b86de59b01d1ece86f7b225b34e8a0
gamer99122 <gamer99122@hotmail.com.tw>
2023-10-04 00:37:00 +0800

    First Commit

---
+++ Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace TestLog4Net
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyWebForm.aspx.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestLog4Net
+{
+    public partial class MyWebForm : System.Web.UI.Page
+    {
+        // Define a static logger variable so that it references the
+        // Logger instance named "MyApp".
+        private static readonly ILog log = LogManager.GetLogger(typeof(MyWebForm));
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Set up a xml configuration that logs.
+            XmlConfigurator.Configure();
+
+            log.Info("Entering application.");
+
+            // do somethings...
+
+            log.Info("Exiting application.");
+        }
+    }
+}
